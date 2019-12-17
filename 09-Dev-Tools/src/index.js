2af1b31b38a447f45cd42b8dd23368fe09f99f72
Ayush Gupta <ayushgupta197@gmail.com>
2019-12-17 15:51:07 +0530

    :heavy_plus_sign: Starter files for challenge-09

---
+++ 09-Dev-Tools/src/index.js
@@ -0,0 +1,18 @@
+const dogs = [{ name: "Snickers", age: 2 }, { name: "hugo", age: 8 }];
+function makeGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
+}
+// Regular
+// Interpolated
+// Styled
+// warning!
+// Error :|
+// Info
+// Testing
+// clearing
+// Viewing DOM Elements
+// Grouping together
+// counting
+// timing
