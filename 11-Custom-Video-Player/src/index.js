dd8f508a981fc81906d62477f2ca27c0b9670541
Ayush Gupta <ayushgupta197@gmail.com>
2019-12-19 10:50:20 +0530

    :art: Added add-in of fullscreen button

--- 11-Custom-Video-Player/src/index.js
+++ 11-Custom-Video-Player/src/index.js
@@ -1,4 +1,4 @@
-/* Get Our Elements */
+// Get elements
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
@@ -6,16 +6,21 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const fullScreen = player.querySelector(".player__fullscreen");
 
-/* Build out functions */
+// Build Functions
 function togglePlay() {
   const method = video.paused ? "play" : "pause";
   video[method]();
+  // if(video.paused) {
+  //   video.play();
+  // } else {
+  //   video.pause();
+  // }
 }
 
 function updateButton() {
-  const icon = this.paused ? "►" : "❚ ❚";
-  console.log(icon);
+  const icon = this.paused ? "►" : "Pause";
   toggle.textContent = icon;
 }
 
@@ -23,8 +28,10 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate() {
+function handleRangeChange() {
   video[this.name] = this.value;
+  console.log(this.value);
+  console.log(this.name);
 }
 
 function handleProgress() {
@@ -37,19 +44,52 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
-/* Hook up the event listeners */
+function handleFullscreen() {
+  if (!isFull) {
+    fullScreen.textContent = "Exit Fullscreen";
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.mozRequestFullScreen) {
+      /* Firefox */
+      player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) {
+      /* IE/Edge */
+      player.msRequestFullscreen();
+    }
+  } else if (isFull) {
+    fullScreen.textContent = "Fullscreen";
+
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (player.mozCancelFullScreen) {
+      player.mozCancelFullScreen();
+    } else if (player.msCancelFullScreen) {
+      player.msCancelFullScreen();
+    }
+  }
+  isFull = !isFull;
+}
+
+// Add evenet listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 video.addEventListener("timeupdate", handleProgress);
-
 toggle.addEventListener("click", togglePlay);
+
 skipButtons.forEach(button => button.addEventListener("click", skip));
-ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
-ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+ranges.forEach(range => range.addEventListener("change", handleRangeChange));
 
 let mousedown = false;
 progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", e => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
+let isFull = false;
+fullScreen.addEventListener("click", handleFullscreen);
