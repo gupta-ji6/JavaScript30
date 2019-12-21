b09de6da56fb134834116225b70e100e350bfb77
Ayush Gupta <ayush.gupta4@cognizant.com>
2019-12-21 11:28:04 +0530

    :bulb: Completed 13-Slide in on scroll

--- 13-Slide-In-On-Scroll/src/index.js
+++ 13-Slide-In-On-Scroll/src/index.js
@@ -13,3 +13,24 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    //half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of the image for when you scroll up
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrollPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrollPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
