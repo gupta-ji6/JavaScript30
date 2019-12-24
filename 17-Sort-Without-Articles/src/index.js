67961f236d5209d1afc53e21d10d4983bce2e495
gupta-ji6 <ayushgupta197@gmail.com>
2019-12-24 04:04:04 -0100

    :heavy_plus_sign: Starter files for challenge-17

---
+++ 17-Sort-Without-Articles/src/index.js
@@ -0,0 +1,15 @@
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog"
+];
