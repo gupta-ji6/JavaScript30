5f98b31570456a59797385818eaeab21d62b49fe
gupta-ji6 <ayushgupta197@gmail.com>
2019-12-25 10:11:12 -0100

    :dizzy: Completed 17-Sort-Without-Articles

--- 17-Sort-Without-Articles/src/index.js
+++ 17-Sort-Without-Articles/src/index.js
@@ -13,3 +13,15 @@ const bands = [
   "Anywhere But Here",
   "An Old Dog"
 ];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector("#bands").innerHTML = sortedBands
+  .map(band => `<li>${band}</li>`)
+  .join("");
+
+console.log(sortedBands);
