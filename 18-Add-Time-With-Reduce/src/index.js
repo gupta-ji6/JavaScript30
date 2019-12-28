f2974a7889acb275cc8f72f87c0c9b23c8a8e716
gupta-ji6 <ayushgupta197@gmail.com>
2019-12-28 10:11:12 -0100

    :seedling: Completed 18-Add-Time-With-Reduce

---
+++ 18-Add-Time-With-Reduce/src/index.js
@@ -0,0 +1,18 @@
+const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft %= 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft %= 60;
+
+console.log(hours, mins, secondsLeft);
