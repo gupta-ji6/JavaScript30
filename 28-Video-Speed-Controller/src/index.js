963c1a5431b1df2292607ec6a32add58bafdc21f
Ayush Gupta <ayushgupta197@gmail.com>
2020-01-13 16:35:04 +0530

    :video_camera: Completed 28-Video-Speed-Controller

---
+++ 28-Video-Speed-Controller/src/index.js
@@ -0,0 +1,18 @@
+const speed = document.querySelector(".speed");
+const bar = speed.querySelector(".speed-bar");
+const video = document.querySelector(".flex");
+
+function handleMove(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + "%";
+  bar.style.height = height;
+
+  const playbackRate = percent * (max - min) + min;
+  bar.textContent = playbackRate.toFixed(2) + "x";
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener("mousemove", handleMove);
