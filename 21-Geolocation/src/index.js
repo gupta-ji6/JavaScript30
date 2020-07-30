68876aa112e669bf50bf79825fae2d16f9ffe564
gupta-ji6 <ayushgupta197@gmail.com>
2020-07-30 17:08:33 +0530

    :ambulance: Speed fix in Geolocation

--- 21-Geolocation/src/index.js
+++ 21-Geolocation/src/index.js
@@ -3,7 +3,7 @@ const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((data) => {
     console.log(data);
-    speed.textContent = data.coords.speed;
+    speed.textContent = (data.coords.speed * 3.6).toFixed(2);
     arrow.style.transform = `rotate(${data.coords.heading}deg)`
 }, (err) => {
     console.log(err);
