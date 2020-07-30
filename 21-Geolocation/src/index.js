d2062d4d342c9784cca8b96f68363a1587f0f053
gupta-ji6 <ayushgupta197@gmail.com>
2020-07-30 16:48:48 +0530

    :pushpin: Completed 21-GeoLocation

---
+++ 21-Geolocation/src/index.js
@@ -0,0 +1,11 @@
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`
+}, (err) => {
+    console.log(err);
+    alert('Uh-oh, you need to allow location permission for this application to run.')
+});
