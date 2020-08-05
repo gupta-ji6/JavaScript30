7df14ecfaa7de81cadd3b9d314ff1aabceb3a68c
gupta-ji6 <ayushgupta197@gmail.com>
2020-08-05 16:00:05 +0530

    :point_up: Added touch to play in JS Drum Kit

--- 01-JS-Drum-Kit/src/index.js
+++ 01-JS-Drum-Kit/src/index.js
@@ -1,6 +1,18 @@
+const keyCodes = {
+  'A': 65,
+  'S': 83,
+  'D': 68,
+  'F': 70,
+  'G': 71,
+  'H': 72,
+  'J': 74,
+  'K': 75,
+  'L': 76,
+};
+
 function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  e.target.classList.remove("playing");
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
 }
 
 function playSound(e) {
@@ -8,11 +20,25 @@ function playSound(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
 
-  key.classList.add("playing");
+  key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
 }
 
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-window.addEventListener("keydown", playSound);
+function playSoundOnTouch(e) {
+  // console.log(e);
+  const audio = document.querySelector(`audio[data-key="${keyCodes[e.path[0].innerText]}"]`);
+  const key = document.querySelector(`div[data-key="${keyCodes[e.path[0].innerText]}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removeTransition);
+  key.addEventListener('touchstart', playSoundOnTouch, false);
+});
+window.addEventListener('keydown', playSound);
