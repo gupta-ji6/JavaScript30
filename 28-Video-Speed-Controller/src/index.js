cc149669efe9cfb0bd91811eb693735983630bbb
gupta-ji6 <ayushgupta197@gmail.com>
2020-08-08 10:10:12 +0100

    :bookmark: Added tag in challenge-28

--- 28-Video-Speed-Controller/src/index.js
+++ 28-Video-Speed-Controller/src/index.js
@@ -6,7 +6,7 @@ function handleMove(e) {
   const y = e.pageY - this.offsetTop;
   const percent = y / this.offsetHeight;
   const min = 0.4;
-  const max = 4;
+  const max = 6;
   const height = Math.round(percent * 100) + "%";
   bar.style.height = height;
 
