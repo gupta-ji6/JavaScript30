8963d144a165b871a0f745ac87b1e55f5400dcdb
gupta-ji6 <ayushgupta197@gmail.com>
2020-08-24 17:31:29 +0530

    :lipstick: Styled challenge-9 better

--- 09-Dev-Tools/src/index.js
+++ 09-Dev-Tools/src/index.js
@@ -1,9 +1,18 @@
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+  p.innerText = "x GO, HAVE A LOOK AT CONSOLE x"
+}
+
 // Regular
 console.log("hello");
 // Interpolated
 console.log("Hello I am a %s string!", "💩");
 // Styled
-// console.log('%c I am some great text', 'font-size:50px; background:red; text-shadow: 10px 10px 0 blue')
+console.log('%c I am some great text', 'font-size:50px; background:red; text-shadow: 10px 10px 0 blue')
 // warning!
 console.warn("OH NOOO");
 // Error :|
@@ -28,21 +37,21 @@ dogs.forEach(dog => {
   console.groupEnd(`${dog.name}`);
 });
 // counting
-console.count("Wes");
-console.count("Wes");
-console.count("Steve");
-console.count("Steve");
-console.count("Wes");
-console.count("Steve");
-console.count("Wes");
-console.count("Steve");
-console.count("Steve");
-console.count("Steve");
-console.count("Steve");
-console.count("Steve");
+console.count("Ayush");
+console.count("Ayush");
+console.count("Gupta");
+console.count("Gupta");
+console.count("Ayush");
+console.count("Gupta");
+console.count("Ayush");
+console.count("Gupta");
+console.count("Gupta");
+console.count("Gupta");
+console.count("Gupta");
+console.count("Gupta");
 // timing
 console.time("fetching data");
-fetch("https://api.github.com/users/wesbos")
+fetch("https://api.github.com/users/gupta-ji6")
   .then(data => data.json())
   .then(data => {
     console.timeEnd("fetching data");
